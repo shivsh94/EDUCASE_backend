8417a627bd895ba6491b575bbfb2297bb74ab860
shivsh94 <shivamsharma23jan@gmail.com>
2025-03-07 02:34:09 +0530

    enhance database configuration to include SSL options for secure connections

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,27 +3,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.MYSQLDATABASE,
-    process.env.MYSQLUSER,
-    process.env.MYSQLPASSWORD,
-    {
-        host: process.env.MYSQLHOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.MYSQLPORT,
-        logging: false,
-    }
-)
+const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST,
+    dialect: process.env.DB_DIALECT || "mysql",
+    port: process.env.MYSQLPORT,
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Required for some hosting services
+        },
+    },
+});
 
 const connectDB = async () => {
     try {
-      await sequelize.authenticate();
-      console.log('✅ Database connected successfully');
+        await sequelize.authenticate();
+        console.log("✅ Database connected successfully");
     } catch (error) {
-      console.error('❌ Unable to connect to the database:', error);
+        console.error("❌ Unable to connect to the database:", error);
     }
-  };
-  
-  connectDB();
-  
-  export default sequelize;
+};
+
+connectDB();
+
+export default sequelize;
