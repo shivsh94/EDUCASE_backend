efc15bc47e81003a28160692eb0507c049b0eb0b
shivsh94 <shivamsharma23jan@gmail.com>
2025-03-07 01:19:11 +0530

    refactor database configuration to use MySQL environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
     {
-        host: process.env.DB_HOST,
+        host: process.env.MYSQLHOST,
         dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
+        port: process.env.MYSQLPORT,
         logging: false,
     }
 )
